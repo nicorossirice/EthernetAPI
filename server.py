e60c7ca7e377304041dbe51829b1526844891894
Nico Rossi <ncr3@rice.edu>
2023-03-07 22:00:07 -0600

    Added SO_REUSEADDR option when binding socket

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ class Server:
 
     def __init__(self, server_port=SERVER_PORT) -> None:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(("", server_port))
         self.sock.listen(5)
 
